58a15d317bfb144a83500b8b7d6cc6c7e45908c4
wozniak04 <mikimen321@gmail.com>
2026-06-10 01:12:11 +0200

    poprawka formatu pliku do bazy

--- init-db.sh
+++ init-db.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 set -e
-
-echo ">>> Rozpoczynanie procesu inicjalizacji bazy danych..."
-
-# Sprawdzenie czy plik jest binarny (Custom Format) czy zwykły SQL
-if pg_restore -l /tmp/app.dump > /dev/null 2>&1; then
-    echo ">>> Wykryto format BINARNY (custom-format). Używam pg_restore..."
-    pg_restore --no-owner -U "$POSTGRES_USER" -d "$POSTGRES_DB" -1 /tmp/app.dump
-else
-    echo ">>> Wykryto format TEKSTOWY (SQL). Używam psql..."
-    psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /tmp/app.dump
-fi
-
-echo ">>> Inicjalizacja bazy zakończona pomyślnie!"
+echo "Odtwarzanie bazy danych z pliku binarnego (custom-format dump)..."
+pg_restore --no-owner -U "$POSTGRES_USER" -d "$POSTGRES_DB" -1 /tmp/app.dump
